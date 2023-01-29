b50585223b10f8d3cec6983c95b56f74dc962eec
rahuladkar <90685828+rahuladkar@users.noreply.github.com>
2023-01-29 15:11:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //  .............. script for the navbar ..............
 
-// const burger = document.querySelector('.burger');
-// const navLinks = document.querySelector('.nav-links');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
 
-// burger.addEventListener('click', () => {
-//     navLinks.classList.toggle('open');
-// });
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
 
 
 //  .............. script for the contact form .............. 
